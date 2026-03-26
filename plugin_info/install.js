4af7c5d11117958d2f35c7eca972cd408ce633c4
Fifirept <ph.lacouchie@proxilan.com>
2026-03-26 07:53:48 +0100

    Delete plugin_info directory

--- plugin_info/install.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-console.log('Plugin installed successfully');
-
-module.exports = {
-  // Export any necessary functions or variables here
-};
--- plugin_info/install.php
+++
@@ -1,7 +0,0 @@
-<?php
-function JeeTizen_install() {
-}
-function JeeTizen_update() {
-}
-function JeeTizen_remove() {
-}
